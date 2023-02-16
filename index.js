b575b992fbc4da95618e2120ec836c3a71dd08b2
Morteza Azimi <64642840+zero0sight@users.noreply.github.com>
2023-02-16 09:32:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send(req.url)
+    res.send(req)
 })
 app.listen(process.env.PORT || 3000)
