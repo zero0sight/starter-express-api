81fdec54a95f86c532f010c48a21302473fad592
Morteza Azimi <64642840+zero0sight@users.noreply.github.com>
2023-02-16 09:43:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send(req.url)
-  
-  if (url.pathname.startsWith('/')) 
-  {
-      let realhostname = "digikala.com";
-      let realpathname = "";
-      url.hostname = realhostname;
-      url.pathname = '/'+ realpathname;
-      url.protocol = "https";
-      let new_request = new Request(url, request);
-      return fetch(new_request);
-    }
+    
+    let realhostname = "digikala.com";
+    let realpathname = "";
+    url.hostname = realhostname;
+    url.pathname = '/'+ realpathname;
+    url.protocol = "https";
+    let new_request = new Request(url, request);
+    return fetch(new_request);
 //     res.send("wtf")
 })
 app.listen(process.env.PORT || 3000)
