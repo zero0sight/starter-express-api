1e3fc61584d84dba9f30186fea41e6342224b6da
Morteza Azimi <64642840+zero0sight@users.noreply.github.com>
2023-02-16 11:42:17 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 const express = require('express')
 const app = express()
+
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send(req.url)
-    
-    let realhostname = "digikala.com";
-    let realpathname = "";
-    url.hostname = realhostname;
-    url.pathname = '/'+ realpathname;
-    url.protocol = "https";
-    let new_request = new Request(url, request);
-    return fetch(new_request);
-//     res.send("wtf")
+//   let url = new URL(req.originalUrl, `https://${req.hostname}`);
+//   let realhostname = url.pathname.split('/')[2];
+//   let realpathname = url.pathname.split('/')[1];
+//   url.hostname = realhostname;
+//   url.pathname = '/' + realpathname;
+//   url.protocol = "https";
+  let newUrl = "digikala.com";
+
+  res.redirect(newUrl);
 })
-app.listen(process.env.PORT || 3000)
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server started on port 3000')
+})
+
+
+// const express = require('express')
+// const app = express()
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     res.send(req.url)
+    
+//     let realhostname = "digikala.com";
+//     let realpathname = "";
+//     url.hostname = realhostname;
+//     url.pathname = '/'+ realpathname;
+//     url.protocol = "https";
+//     let new_request = new Request(url, request);
+//     return fetch(new_request);
+// //     res.send("wtf")
+// })
+// app.listen(process.env.PORT || 3000)
 
 
 
