a01121392d33775c69c7d0fb03773e68280e1f1f
Morteza Azimi <64642840+zero0sight@users.noreply.github.com>
2023-02-16 09:36:40 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send(req.url)
-//     res.send("wtf")
-})
-app.listen(process.env.PORT || 3000)
+// const express = require('express')
+// const app = express()
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     res.send(req.url)
+// //     res.send("wtf")
+// })
+// app.listen(process.env.PORT || 3000)
+
+
+
+export default {
+  async fetch(request, env) {
+    let url = new URL(request.url);
+    if (url.pathname.startsWith('/')) {
+      let realhostname = "morteza.persiangolf.click";
+      let realpathname = "pepsi";
+      url.hostname = realhostname;
+      url.pathname = '/'+ realpathname;
+      url.protocol = "https";
+      let new_request = new Request(url, request);
+      return fetch(new_request);
+    }
+    return env.ASSETS.fetch(request);
+  },
+};
